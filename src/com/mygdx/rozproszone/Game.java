06572eecb3830f0411ef32dc8639c5f675fd869f
NerkiTrzy <przemek.roguski@wp.pl>
2016-04-18 13:53:37 +0200

    Collision flags converted to operation on variables.

--- src/com/mygdx/rozproszone/Game.java
+++ src/com/mygdx/rozproszone/Game.java
@@ -16,12 +16,12 @@ public class Game extends ApplicationAdapter {
     private Player player1;
     private Level level;
     
-    private float angle =360.0f;
-    private float angleDelta = 5;
-    private float oldX;
-    private float oldY;
-    private float newX;
-    private float newY;
+    private Float angle =360.0f;
+    private Float angleDelta = 5.0f;
+    private Float oldX;
+    private Float oldY;
+    private Float newX;
+    private Float newY;
     boolean velocityFlag = false;
     boolean keyPressedFlag;
     
@@ -61,60 +61,52 @@ public class Game extends ApplicationAdapter {
         if(Gdx.input.isKeyPressed(Keys.UP))
         {
             velocityFlag = true;
-            newX = (float) (Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
-            newY = (float) (Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+            newX = new Float (Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+            newY = new Float (Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
             checkCollision(newX,newY);
-            if (player1.getAbleToMove()) {
-                checkSlowDust(newX,newY);
-                if (player1.getSlowedDust()) {
-                    newX/=2;
-                    newY/=2;
-                }
-                player1.changePosition(newX,newY );
-                if(Gdx.input.isKeyPressed(Keys.RIGHT))
-                {
-                    player1.getCarImage().rotate(-angleDelta);
-                    angle += angleDelta;
-                }
-                else if(Gdx.input.isKeyPressed(Keys.LEFT))
-                {
-                    player1.getCarImage().rotate(angleDelta);
-                    angle -= angleDelta;
-                }
-                if(player1.getVelocity() < 300) player1.changeVelocity(10);
-                
-                if(player1.getVelocity()> 150)angleDelta = (float) (600/player1.getVelocity());
-            }
+            checkSlowDust(newX,newY);
             
+            player1.changePosition(newX,newY );
+            if(Gdx.input.isKeyPressed(Keys.RIGHT))
+            {
+                player1.getCarImage().rotate(-angleDelta);
+                angle += angleDelta;
+            }
+            else if(Gdx.input.isKeyPressed(Keys.LEFT))
+            {
+                player1.getCarImage().rotate(angleDelta);
+                angle -= angleDelta;
+            }
+            if(player1.getVelocity() < 300) player1.changeVelocity(10);
+            
+            if(player1.getVelocity()> 150)angleDelta = new Float (600/player1.getVelocity());
         }
+        
+        
         else if (Gdx.input.isKeyPressed(Keys.DOWN))
         {
             velocityFlag = false;
-            newX = (float) (-Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
-            newY = (float) (-Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+            newX = new Float (-Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+            newY = new Float (-Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
             checkCollision(newX,newY);
-            if (player1.getAbleToMove()) {
-                checkSlowDust(newX,newY);
-                if (player1.getSlowedDust()) {
-                    newX/=2;
-                    newY/=2;
-                }
-                player1.changePosition(newX, newY);
-                if(Gdx.input.isKeyPressed(Keys.RIGHT))
-                {
-                    player1.getCarImage().rotate(-angleDelta);
-                    angle += angleDelta;
-                }
-                else if(Gdx.input.isKeyPressed(Keys.LEFT))
-                {
-                    player1.getCarImage().rotate(angleDelta);
-                    angle -= angleDelta;
-                }
-                
-                if(player1.getVelocity() < 100) player1.changeVelocity(5);
-                
-                if(player1.getVelocity() > 50)angleDelta = (float) (600/player1.getVelocity());
+            checkSlowDust(newX,newY);
+            
+            player1.changePosition(newX, newY);
+            if(Gdx.input.isKeyPressed(Keys.RIGHT))
+            {
+                player1.getCarImage().rotate(-angleDelta);
+                angle += angleDelta;
             }
+            else if(Gdx.input.isKeyPressed(Keys.LEFT))
+            {
+                player1.getCarImage().rotate(angleDelta);
+                angle -= angleDelta;
+            }
+            
+            if(player1.getVelocity() < 100) player1.changeVelocity(5);
+            
+            if(player1.getVelocity() > 50)angleDelta = new Float (600/player1.getVelocity());
+            
             
         }
         else if(player1.getVelocity() > 0)
@@ -122,60 +114,52 @@ public class Game extends ApplicationAdapter {
             if (player1.getAbleToMove()) {
                 
                 if (velocityFlag) {
-                    newX = (float) (Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
-                    newY = (float) (Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+                    newX = new Float (Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+                    newY = new Float (Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
                     player1.changeVelocity(-10);
                     checkCollision(newX,newY);
                     
-                    if (player1.getAbleToMove()) {
-                        checkSlowDust(newX,newY);
-                        if (player1.getSlowedDust()) {
-                            newX/=2;
-                            newY/=2;
-                        }
-                        player1.changePosition(newX, newY);
-                        //player1.setPositionX((float) (player1.getPositionX()+Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
-                        // player1.setPositionY((float) (player1.getPositionY()+Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
-                        
-                        if(Gdx.input.isKeyPressed(Keys.RIGHT))
-                        {
-                            player1.getCarImage().rotate(-1);
-                            angle += 1;
-                        }
-                        else if(Gdx.input.isKeyPressed(Keys.LEFT))
-                        {
-                            player1.getCarImage().rotate(1);
-                            angle -= 1;
-                        }
+                    checkSlowDust(newX,newY);
+                    
+                    player1.changePosition(newX, newY);
+                    //player1.setPositionX((Float) (player1.getPositionX()+Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
+                    // player1.setPositionY((Float) (player1.getPositionY()+Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
+                    
+                    if(Gdx.input.isKeyPressed(Keys.RIGHT))
+                    {
+                        player1.getCarImage().rotate(-1);
+                        angle += 1;
                     }
+                    else if(Gdx.input.isKeyPressed(Keys.LEFT))
+                    {
+                        player1.getCarImage().rotate(1);
+                        angle -= 1;
+                    }
+                    
                     
                 }
                 else{
                     player1.changeVelocity(-5);
-                    newX = (float) (-Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
-                    newY = (float) (-Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime());
+                    newX = new Float((-Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
+                    newY = new Float((-Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
                     checkCollision(newX,newY);
-                    if (player1.getAbleToMove()) {
-                        checkSlowDust(newX,newY);
-                        if (player1.getSlowedDust()) {
-                            newX/=2;
-                            newY/=2;
-                        }
-                        player1.changePosition(newX, newY);
-                        // player1.setPositionX((float) (player1.getPositionX()-Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
-                        //player1.setPositionY((float) (player1.getPositionY()-Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
-                        
-                        if(Gdx.input.isKeyPressed(Keys.RIGHT))
-                        {
-                            player1.getCarImage().rotate(-1);
-                            angle += 1;
-                        }
-                        else if(Gdx.input.isKeyPressed(Keys.LEFT))
-                        {
-                            player1.getCarImage().rotate(1);
-                            angle -= 1;
-                        }
+                    checkSlowDust(newX,newY);
+                    
+                    player1.changePosition(newX, newY);
+                    // player1.setPositionX((Float) (player1.getPositionX()-Math.sin(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
+                    //player1.setPositionY((Float) (player1.getPositionY()-Math.cos(angle*Math.PI/180)*player1.getVelocity()*Gdx.graphics.getDeltaTime()));
+                    
+                    if(Gdx.input.isKeyPressed(Keys.RIGHT))
+                    {
+                        player1.getCarImage().rotate(-1);
+                        angle += 1;
                     }
+                    else if(Gdx.input.isKeyPressed(Keys.LEFT))
+                    {
+                        player1.getCarImage().rotate(1);
+                        angle -= 1;
+                    }
+                    
                 }
                 
                 
@@ -190,27 +174,24 @@ public class Game extends ApplicationAdapter {
         level.updateView();
     }
     
-    void checkCollision(float x, float y){
+    void checkCollision(Float x, Float y){
         for (int i = 0; i < level.getCollisionObjects().getCount(); i++) {
             RectangleMapObject obj = (RectangleMapObject) level.getCollisionObjects().get(i);
             Rectangle rect = obj.getRectangle();
             Rectangle recPlayer = new Rectangle();
-            recPlayer.set(player1.getPositionX()+x,player1.getPositionY()+y,player1.getCarImage().getWidth(),player1.getCarImage().getHeight());
+            recPlayer.set(player1.getPositionX()+x,player1.getPositionY()+y,player1.getCarImage().getWidth()-6,player1.getCarImage().getHeight()-20);
             
             
             if (recPlayer.overlaps(rect)) {
-                player1.setAbleToMove(false);
+                newX = 0.0f;
+                newY = 0.0f;
                 break;
             }
-            else {
-                player1.setAbleToMove(true);
-            }
-            
             
         }
     }
     
-    void checkSlowDust(float x, float y){
+    void checkSlowDust(Float x, Float y){
         for (int i = 0; i < level.getSlowDustObjects().getCount(); i++) {
             RectangleMapObject obj = (RectangleMapObject) level.getSlowDustObjects().get(i);
             Rectangle rect = obj.getRectangle();
@@ -219,14 +200,10 @@ public class Game extends ApplicationAdapter {
             
             
             if (recPlayer.overlaps(rect)) {
-                player1.setSlowedDust(true);
+                newX /=2;
+                newY /=2;
                 break;
             }
-            else {
-                player1.setSlowedDust(false);
-            }
-            
-            
         }
     }
 }
